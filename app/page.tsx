66f7728d3ceb2bda5c8451d90211f3801ceb3b6b
Youssefbenarbiya <youssefbenarbia345@gmail.com>
2025-01-30 17:00:31 +0100

    Add sign-in, sign-up, and password reset functionalities

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import { auth } from "@/auth"
+import { headers } from "next/headers"
+
 export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
   return (
     <main className="flex items-center justify-center grow p-8">
       <div className="flex flex-col items-center gap-4">
         <h1 className="text-7xl">Hello</h1>
-        <p>You are logged in as: </p>
+        <p>You are logged in as: {session?.user?.email}</p>
       </div>
     </main>
   )
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,22 +1,13 @@
-import { Button } from "@/components/ui/button"
 import Link from "next/link"
-export default async function Navbar() {
+import AuthButtons from "@/components/auth-buttons"
 
+export default function Navbar() {
   return (
-    <nav className="justify-between items-center py-3 px-4 fixed top-0 left-0 right-0 z-50">
-      <Link href="/" className="font-bold">
+    <nav className="flex justify-between items-center py-3 px-4 fixed top-0 left-0 right-0 z-50 bg-slate-100">
+      <Link href="/" className="text-xl font-bold">
         better-auth
       </Link>
-     
-        <div className="gap-2 justify-center">
-          <Link href="/login">
-            <Button>Sign In</Button>
-          </Link>
-          <Link href="/sign-up">
-            <Button>Sign Up</Button>
-          </Link>
-        </div>
-      
+      <AuthButtons />
     </nav>
   )
 }
