4eb51efa8830c2ed8b041471f573ea5dc5a9c3ef
Youssefbenarbiya <youssefbenarbia345@gmail.com>
2025-04-11 17:21:57 +0100

    feat: refactor CarFilter component for improved filtering and search functionality

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,19 +1,20 @@
 // import { drizzle } from "drizzle-orm/postgres-js"
-// import * as schema from "./schema"
 // import postgres from "postgres"
+// import * as schema from "./schema"
 
-// // Initialize the Postgres client using POSTGRES_URL
-// const sql = postgres(process.env.POSTGRES_URL!, {
-//   debug: true,
-//   ssl: false, // Set to `true` if connecting to a production database with SSL
+// // Initialize the Postgres client using DATABASE_URL for Docker
+// const sql = postgres(process.env.DATABASE_URL!, {
+//   max: 10,
+//   ssl: false, // No SSL for local Docker connection
 // })
 
 // const db = drizzle(sql, {
 //   schema,
-//   logger: true,
+//   logger: process.env.NODE_ENV === "development",
 // })
 
 // export default db
+
 import { drizzle } from "drizzle-orm/neon-http"
 import { neon } from "@neondatabase/serverless"
 import * as schema from "./schema"
